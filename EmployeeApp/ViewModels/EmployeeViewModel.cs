76c7da52c07d756ee45acb5503be47c1774ccadc
Jansan <jansan-26@hotmail.com>
2019-05-29 19:15:14 +0200

    EmployeeViewModel created

---
+++ EmployeeApp/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApp.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
