6be4b06b7543cc0828f2bfc5b131ddaed3ce0f55
Jansan <jansan-26@hotmail.com>
2019-05-29 22:07:53 +0200

    EmployeeController Created

---
+++ EmployeeApp/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApp.Models;
+using EmployeeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<EmployeeViewModel> list = new List<EmployeeViewModel>();
+            using (EmployeeAppEntities ea = new EmployeeAppEntities())
+            {
+                var v = (from a in ea.Employees
+                         join b in ea.EmployeeRoles on a.RoleID equals b.ID
+                         select new EmployeeViewModel
+                         {
+                             ID = a.ID,
+                             FirstName = a.FirstName,
+                             LastName = a.LastName,
+                             DateOfBirth = a.DateOfBirth,
+                             RoleID = a.RoleID,
+                             RoleName = b.RoleName
+                         });
+                list = v.ToList();
+            }
+            return View(list);
+        }
+    }
+}
